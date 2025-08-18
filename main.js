a4843d88f4a3166100c185ba9d711658edf6292a
Teeheene <diane.panganiban1010@gmail.com>
2025-08-18 15:47:57 +0800

    feat: implement rgb mode

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 //Written by Teehee
 
+//settings
+const emptyCellColor = "white";
+const takenCellColor = "black";
+
 //get the grid size
 const confirmGridBtn = document.querySelector("#bsize");
 
@@ -23,9 +27,21 @@ confirmGridBtn.addEventListener("click", () => {
 
 //set up grid
 function clearGrid(parent) {
-	while(parent.firstChild) {
-		parent.removeChild(parent.firstChild);
-	}
+	parent.innerHTML = "";
+}
+
+function clearGridDrawing(parent) {
+	const cellsY = [...parent.childNodes];
+
+	cellsY.forEach(cellsX => {
+		cells = [...cellsX.childNodes];
+
+		//remove all the class styles indicated
+		cells.forEach(cell => {
+			cell.style.backgroundColor = emptyCellColor;
+			cell.style.removeProperty("opacity");
+		});
+	});
 }
 
 function setGridSize(size) {
@@ -49,22 +65,90 @@ function setGridSize(size) {
 
 //event listeners for grid
 function playGrid() {
+	console.log("playing classic grid...");
 	const container = document.getElementById("container");
 	const cellsY = container.children;
-	
-	console.log(cellsY);
-	console.log(cellsY.length);
 
+	clearGridDrawing(container);
+
+	//UNO////////////////////////////////////////////
+	[...cellsY].forEach(cellsX => {
+		[...cellsX.childNodes].forEach(cell => {
+			cell.addEventListener("mouseover", () => {
+				cell.style.backgroundColor = takenCellColor;
+			})
+		})
+	});
+
+	//DOS/////////////////////////////////////////////
+	/*
 	for(let i = 0; i < cellsY.length; i++) {
 		const cellsX = cellsY[i].childNodes;
 		for(let j = 0; j < cellsX.length; j++) {
 			cellsX[j].addEventListener("mouseover", () => {
 				cellsX[j].style.backgroundColor = "black";	
-			})
+			});
 		}
 	}
+	*/
 }
 
-//initialize grid
+function getRandomRgbColor() {
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+function playRgbGrid() {
+	console.log("playing rgb grid...");
+	const container = document.getElementById("container");
+	const cellsY = container.children;
+
+	clearGridDrawing(container);
+
+	[...cellsY].forEach(cellsX => {
+		[...cellsX.childNodes].forEach(cell => {
+			cell.addEventListener("mouseover", () => {
+				cell.style.backgroundColor = getRandomRgbColor();
+			});
+		});
+	});
+}
+
+function playDarkeningGrid() {
+	console.log("playing darkening grid...");
+}
+
+//settings
+//gets the reference to all settings children and spreads it into an array
+const settingsBtn = [...document.getElementById("settings").children];
+
+
+settingsBtn.forEach((btn, index) => {
+	console.log(btn.textContent);
+	btn.addEventListener("click", () => {
+		settingsBtn.forEach(btn => {
+			btn.classList.remove("highlight");
+		})
+		btn.classList.add("highlight");
+
+		switch(btn.textContent) {
+			case "rgb mode": 
+				playRgbGrid();
+				break;
+			case "classic":
+				playGrid();
+				break;
+			case "darkening mode":
+				playDarkeningGrid();
+				break;
+		}
+	})
+});
+
+//DEFAULT GAME
+settingsBtn[1].classList.add("highlight");
 setGridSize(64);
 playGrid();
